1a07012a5db13abb7f7f60ec60ea4165ea000034
Eric Feng <ericfeng610@gmail.com>
2020-04-04 20:02:42 -0600

    replaced error handler with helper

--- app.js
+++ app.js
@@ -8,6 +8,8 @@ var logger = require('morgan');
 
 require('dotenv').config();
 
+var { handleError } = require('./helpers/error');
+
 var db = require('./connection/connection');
 db.connectDB();
 
@@ -43,13 +45,7 @@ app.use(function(req, res, next) {
 
 // error handler
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+  handleError(err, res);
 });
 
 module.exports = app;
