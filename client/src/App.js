29d88383a1d9b8f3e004fcec8fd8d8a96baa9a53
Eric Feng <ericfeng610@gmail.com>
2020-04-30 18:36:40 -0600

    initial commit for displaying pdf

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,43 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Document, Page } from "react-pdf/dist/entry.webpack";
 
-function App() {
+class App extends Component {
+
+  componentDidMount() {
+    fetch('http://localhost:8000/documents', {
+      method: "GET",
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI1ZTg2ZDRjYWUzMzgzYzVkOTllMGY1NTkiLCJpYXQiOjE1ODgyOTEzNTQsImV4cCI6MTU4ODMzNDU1NH0.tXcfF42Si6IgnYQCTIY_VEYZIdtdrPwaWjussdcCsJU'
+
+      }
+    })
+    .then(res => res.json())
+    .then((data) => {
+      console.log(data)
+    })
+    .catch((err) => {
+      console.log(err)
+    });
+      
+  }
+  render() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.js</code> and sasve to reload.
         </p>
+        <Document
+          file = "https://s3-us-east-2.amazonaws.com/penguins-are-cool/Eric Feng Resume.pdf"
+          onLoadError={console.error}
+          onLoadSuccess={console.log}
+        >
+        <Page pageNumber={1} />
+        </Document>
         <a
           className="App-link"
           href="https://reactjs.org"
@@ -21,6 +49,7 @@ function App() {
       </header>
     </div>
   );
+  }
 }
 
 export default App;
