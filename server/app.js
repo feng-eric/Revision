6d3e2298646ff4580d0bbbb045b6a19882eac7d6
Eric Feng <ericfeng610@gmail.com>
2020-05-13 00:50:48 -0600

    reverted change

--- server/app.js
+++ server/app.js
@@ -41,11 +41,8 @@ app.use('/documents', documentRouter);
 app.use('/comments', commentRouter);
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/build')));
+  app.use(express.static(path.join(__dirname, '../client/build/index.html')));
 
-  app.get('*', function(req, res) {
-    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
-  });
 }
 
 // catch 404 and forward to error handler
