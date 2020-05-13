b997153e4f028aa94857de08cf3871e3021a94ee
Eric Feng <ericfeng610@gmail.com>
2020-05-13 00:55:40 -0600

    test

--- server/app.js
+++ server/app.js
@@ -41,7 +41,11 @@ app.use('/documents', documentRouter);
 app.use('/comments', commentRouter);
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/build/index.html')));
+  app.use(express.static(path.join(__dirname, '../client/build')));
+
+  app.get('/*', function (req, res) {
+    res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
+  });
 
 }
 
